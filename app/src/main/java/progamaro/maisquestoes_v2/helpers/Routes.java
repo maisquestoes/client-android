ddf088781aaa998e35a79b6890960de726d34d25
heliofeliciano <heliofeliciano@gmail.com>
2015-10-29 21:29:03 -0300

    Route question

--- app/src/main/java/progamaro/maisquestoes_v2/helpers/Routes.java
+++ app/src/main/java/progamaro/maisquestoes_v2/helpers/Routes.java
@@ -7,4 +7,5 @@ public class Routes {
     public static final String SIGNUP = "http://api2.maisquestoes.com.br/auth/signup/";
     public static final String SIGNIN = "http://api2.maisquestoes.com.br/auth/signin/";
     public static final String ALL_SUBJECTS = "http://api2.maisquestoes.com.br/subjects/";
+    public static final String QUESTION = "http://api2.maisquestoes.com.br/questions/";
 }
